a41e0f9f99b997fdd7c69ca0dc936f28e6a5008b
Jonaljung1991 <32454645+Jonaljung1991@users.noreply.github.com>
2018-04-15 14:35:27 +0200

    first shot

---
+++ src/components/Createcontact.vue
@@ -0,0 +1,40 @@
+<template>
+
+<div>
+<ul>
+<li>{{name}}</li>
+    <li>{{email}}</li>
+    <li>{{age}}</li>
+</ul>    
+</div>
+
+</template>
+
+
+
+
+<script>
+export default {
+    data: function(){
+        return{
+            name:"Jonathan",
+            email:"jontecoolboy@blabla.com",
+            age:"12"
+        }
+    }
+};
+
+</script>
+
+
+<style scoped>
+    div{
+        width: 100vw;
+        height: 15vh;
+        background-color: powderblue;
+    }
+    ul{
+        list-style: none;
+    }
+</style>
+
